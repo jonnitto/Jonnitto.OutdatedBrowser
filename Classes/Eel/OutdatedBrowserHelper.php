38cf98a74c06d8a1bcba035f9f4e31190f965da9
Jon Uhlmann <account@uhlmann.pro>
2021-11-11 13:12:49 +0100

    Update: Change close anchor to button
    Fix #1

--- Classes/Eel/OutdatedBrowserHelper.php
+++ Classes/Eel/OutdatedBrowserHelper.php
@@ -24,6 +24,38 @@ class OutdatedBrowserHelper implements ProtectedContextAwareInterface
     }
 
     /**
+     * Clean up markup from template
+     *
+     * @param string $markup
+     * @return string
+     */
+    public function cleanMarkup(string $markup): string
+    {
+
+        return str_replace(
+            [
+                'id="btnUpdateBrowser"',
+                'http://outdatedbrowser.com',
+                '<h6>',
+                '</h6>',
+                ' class="last"',
+                '<a href="#" id="btnCloseUpdateBrowser"',
+                '>&times;</a>'
+            ],
+            [
+                'id="btnUpdateBrowser" rel="nofollow noopener" target="_blank"',
+                'https://bestvpn.org/outdatedbrowser',
+                '<p><strong>',
+                '</strong></p>',
+                ' class="outdated-last"',
+                '<button type="button" id="btnCloseUpdateBrowser"',
+                '>&times;</button>'
+            ],
+            (string)$markup
+        );
+    }
+
+    /**
      * All methods are considered safe
      *
      * @param string $methodName
